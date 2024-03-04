c81cc156129153377f70efa03c963f48ece234f1
Templum <templum.dev@gmail.com>
2024-03-04 21:27:24 +0000

    :construction: Added temporary plans

---
+++ lib/lib.ts
@@ -0,0 +1,5 @@
+// TODO: Export Decorators
+
+// IDEA: Metric Decorators: Counter, Gauge, Histogram based on Prometheus [https://github.com/siimon/prom-client]
+// IDEA: Cache Decorator: Memory + Adapter
+// IDEA: Rate Limit
