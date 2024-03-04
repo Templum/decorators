3339d006c5535a53e0ebaaff357fcccd53ce69bf
Templum <templum.dev@gmail.com>
2024-03-04 21:26:37 +0000

    :sparkles: Implemented Retry Decorator
    It ensures the return type stays unchanged.

---
+++ lib/execution/retry.ts
@@ -0,0 +1,119 @@
+import { isPromiseLike } from '../util/predicates';
+import { convertFrom } from '../util/transfomers';
+
+import { MethodDecorator, UnitOfTime } from '../util/types.js';
+
+/**
+ * Retry Strategy for more details on the functions look at the individual members
+ */
+export enum RetryStrategy {
+    /**
+     * Calls will be made with a expontential growing delay waiting between attempts.
+     * This is useable by functions that return promise or are async. Sync methods can not use it.
+     */
+    Exponential,
+    /**
+     * Calls will be made with a fixed delay between attempts.
+     * This is useable by functions that return promise or are async. Sync methods can not use it.
+     */
+    Delay,
+    /**
+     * Calls will be made sequential without explicit waiting time between them.
+     * Sync methods only support this strategy as Exponential would change the return type.
+     */
+    Sequential,
+}
+
+/**
+ * Allows to configure the retry behaviour
+ */
+export type RetryConfig = {
+    /**
+     * Defaults to 3
+     */
+    retries: number;
+    /**
+     * Defaults to RetryStrategy.Delay, only relevant for Promise/Async Methods
+     */
+    strategy: RetryStrategy;
+    /**
+     * Defaults to 100, only relevant with proper RetryStrategy
+     */
+    delay: number;
+    /**
+     * Default to MS, only relevant with proper RetryStrategy
+     */
+    unit: Omit<UnitOfTime, 'Nanosecond'>;
+};
+
+const defaultConfig: RetryConfig = {
+    retries: 3,
+    strategy: RetryStrategy.Delay,
+    delay: 100,
+    unit: UnitOfTime.Millisecond,
+};
+
+export function Retry(isRetrieable: (error: Error) => boolean, config: Partial<RetryConfig> = {}): MethodDecorator {
+    const { retries, delay, unit, strategy } = Object.assign(defaultConfig, config);
+
+    const delayInMs = convertFrom(delay, UnitOfTime.Millisecond, unit as UnitOfTime);
+
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    return (target: Function, _ctx: ClassMethodDecoratorContext) => {
+        return (self: unknown, ...args: unknown[]): unknown => {
+            const handleSyncRetries = (): unknown => {
+                for (let attempt = 0; attempt < retries; attempt++) {
+                    try {
+                        const result: unknown = target.call(self, args);
+                        return result;
+                    } catch (error) {
+                        if (!isRetrieable(error as Error) || attempt + 1 >= retries) {
+                            throw error;
+                        }
+                    }
+                }
+            };
+
+            const handleAsyncRetries = async (): Promise<unknown> => {
+                for (let attempt = 0; attempt < retries; attempt++) {
+                    try {
+                        const result: Promise<unknown> = target.call(self, args) as Promise<unknown>;
+                        return await result;
+                    } catch (error) {
+                        if (!isRetrieable(error as Error) || attempt + 1 >= retries) {
+                            throw error;
+                        }
+
+                        if (strategy === RetryStrategy.Delay) {
+                            await new Promise<void>((resolve) => setTimeout(resolve, delayInMs));
+                        }
+
+                        if (strategy === RetryStrategy.Exponential) {
+                            await new Promise<void>((resolve) => setTimeout(resolve, delayInMs * (attempt + 1)));
+                        }
+                    }
+                }
+            };
+
+            try {
+                const result: unknown = target.call(self, args);
+
+                if (isPromiseLike(result)) {
+                    return new Promise((resolve, reject) => {
+                        result
+                            .then((response) => resolve(response))
+                            .catch((_error) =>
+                                handleAsyncRetries()
+                                    .then((response) => resolve(response))
+                                    .catch((error) => reject(error)),
+                            );
+                    });
+                }
+
+                return result;
+            } catch (_error) {
+                return handleSyncRetries();
+            }
+        };
+    };
+}
