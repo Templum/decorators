d0854c3b3e0c461701d7c7f11a83b196e053b200
Templum <templum.dev@gmail.com>
2024-03-07 22:34:18 +0000

    :bug: Fixed Issues with Debounce Decorator
    Both with Typings and Operation

--- lib/execution/debounce.ts
+++ lib/execution/debounce.ts
@@ -1,5 +1,13 @@
-import { MethodDecorator, UnitOfTime } from '../util/types.js';
+import { UnitOfTime } from '../util/types.js';
 import { convertFrom } from '../util/transfomers.js';
+
+type DebounceDecorator = <TThis, TArgs extends unknown[], Return extends void>(
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    target: Function,
+    ctx: ClassMethodDecoratorContext<TThis, (this: TThis, ...args: TArgs) => Return>,
+) => void;
+type BeforePatch = { _timeToNextCall_: number | undefined };
+type AfterPatch = { _timeToNextCall_: number };
 
 /**
  * A Method Decorator that debounces additional calls beig made to the decorated target. If the debounce
@@ -7,15 +15,23 @@ import { convertFrom } from '../util/transfomers.js';
  * @param time value
  * @param unit of the provided timing
  */
-export function Debounce(time: number, unit: Exclude<UnitOfTime, 'Nanosecond'>): MethodDecorator {
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    return (target: Function, _ctx: ClassMethodDecoratorContext) => {
-        let timeToNextCall = 0;
+export function Debounce(time: number, unit: Exclude<UnitOfTime, 'Nanosecond'>): DebounceDecorator {
+    return <TThis, TArgs extends unknown[], Return extends void>(
+        // eslint-disable-next-line @typescript-eslint/ban-types
+        target: Function,
+        _ctx: ClassMethodDecoratorContext<TThis, (this: TThis, ...args: TArgs) => Return>,
+    ) => {
+        // TODO: Check if the patching can be moved to initializer
 
-        return (self: unknown, ...args: unknown[]): unknown => {
-            if (Date.now() >= timeToNextCall) {
-                timeToNextCall = Date.now() + convertFrom(time, UnitOfTime.Millisecond, unit);
-                return target.call(self, args);
+        // eslint-disable-next-line @typescript-eslint/ban-types
+        return function (this: TThis, self: Function, ...args: unknown[]): void {
+            if ((this as BeforePatch)._timeToNextCall_ === undefined) {
+                (this as AfterPatch)._timeToNextCall_ = 0;
+            }
+
+            if (Date.now() >= (this as AfterPatch)._timeToNextCall_) {
+                (this as AfterPatch)._timeToNextCall_ = Date.now() + convertFrom(time, UnitOfTime.Millisecond, unit);
+                return self === undefined ? (target.call(this, args) as Return) : (target.call(self, args) as Return);
             }
             return;
         };
--- lib/util/types.ts
+++ lib/util/types.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/ban-types
-export type MethodDecorator = (target: Function, ctx: ClassMethodDecoratorContext) => (this: unknown, ...args: unknown[]) => unknown;
+export type MethodDecorator = (target: Function, ctx: ClassMethodDecoratorContext) => <Fn, Arg, Result>(self: Fn, ...args: Arg[]) => Result;
 
 /**
  * The supported unit of times. Some decorators may exclude Nanosecond given
