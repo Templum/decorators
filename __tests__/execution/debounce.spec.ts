679e93d16a88fc29b50c7f7001cb2863b293e315
Templum <templum.dev@gmail.com>
2024-03-09 23:29:43 +0000

    :recycle: Uses jest.mock for call recording

--- __tests__/execution/debounce.spec.ts
+++ __tests__/execution/debounce.spec.ts
@@ -2,65 +2,51 @@ import { Debounce } from "../../lib/execution/debounce.js";
 import { UnitOfTime } from "../../lib/util/types.js";
 
 class Test {
-    constructor(public counter: number = 0){}
+    constructor(private spy: jest.Mock){}
 
     @Debounce(5, UnitOfTime.Millisecond)
     public increment(): void {
-        this.counter += 1;
-    }
-
-    public setCounter(value: number): void {
-        this.counter = value;
-    }
-
-    public getCounter(): number {
-        return this.counter;
+        this.spy();
     }
 }
 
 describe('Debounce Decorator', () => {
     test('should call through if not in debounce window', async () => {
-        const target = new Test();
-        const before = target.getCounter();
+        const callRecorder = jest.fn();
+        const target = new Test(callRecorder);
 
         target.increment();
-
-        expect(before).toEqual(0);
-        expect(target.getCounter()).toEqual(1);    
+        expect(callRecorder).toHaveBeenCalledTimes(1); 
     });
 
     test('should block additional calls while in debounce window', async () => {
-        const target = new Test();
-        const before = target.getCounter();
+        const callRecorder = jest.fn();
+        const target = new Test(callRecorder);
 
         target.increment();
-        
         target.increment();
         target.increment();
         target.increment();
         target.increment();
 
-        const delta = target.getCounter() - before;
-        expect(before).toEqual(0);
-        expect(delta).toEqual(1);
+        expect(callRecorder).toHaveBeenCalledTimes(1); 
     });
 
     test('should allow a call after debounce window passed', async () => {
-        const target = new Test();
+        const callRecorder = jest.fn();
+        const target = new Test(callRecorder)
 
         target.increment();
         target.increment();
         target.increment();
 
-        const beforeWindow = target.getCounter();
-        expect(beforeWindow).toEqual(1);
+        expect(callRecorder).toHaveBeenCalledTimes(1); 
         await new Promise((resolve) => setTimeout(resolve, 10));
 
         target.increment();
         target.increment();
         target.increment();
 
-        const afterWindow = target.getCounter();
-        expect(afterWindow).toEqual(2);
+        expect(callRecorder).toHaveBeenCalledTimes(2); 
     });
 });
