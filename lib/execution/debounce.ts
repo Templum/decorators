e6db661bb9872e64368e47c2ac3935be1621e194
Templum <templum.dev@gmail.com>
2024-03-09 12:14:22 +0000

    :bug: Smaller fixes to type + calling

--- lib/execution/debounce.ts
+++ lib/execution/debounce.ts
@@ -6,8 +6,6 @@ type DebounceDecorator = <TThis, TArgs extends unknown[], Return extends void>(
     target: Function,
     ctx: ClassMethodDecoratorContext<TThis, (this: TThis, ...args: TArgs) => Return>,
 ) => void;
-type BeforePatch = { _timeToNextCall_: number | undefined };
-type AfterPatch = { _timeToNextCall_: number };
 
 /**
  * A Method Decorator that debounces additional calls beig made to the decorated target. If the debounce
@@ -19,19 +17,19 @@ export function Debounce(time: number, unit: Exclude<UnitOfTime, 'Nanosecond'>):
     return <TThis, TArgs extends unknown[], Return extends void>(
         // eslint-disable-next-line @typescript-eslint/ban-types
         target: Function,
-        _ctx: ClassMethodDecoratorContext<TThis, (this: TThis, ...args: TArgs) => Return>,
+        ctx: ClassMethodDecoratorContext<TThis, (this: TThis, ...args: TArgs) => Return>,
     ) => {
-        // TODO: Check if the patching can be moved to initializer
+        const timeToNextCall = Symbol.for('timeToNextCall');
+        type Patched = TThis & { [timeToNextCall]: number };
 
-        // eslint-disable-next-line @typescript-eslint/ban-types
-        return function (this: TThis, self: Function, ...args: unknown[]): void {
-            if ((this as BeforePatch)._timeToNextCall_ === undefined) {
-                (this as AfterPatch)._timeToNextCall_ = 0;
-            }
+        ctx.addInitializer(function () {
+            (this as Patched)[timeToNextCall] = 0;
+        });
 
-            if (Date.now() >= (this as AfterPatch)._timeToNextCall_) {
-                (this as AfterPatch)._timeToNextCall_ = Date.now() + convertFrom(time, UnitOfTime.Millisecond, unit);
-                return self === undefined ? (target.call(this, args) as Return) : (target.call(self, args) as Return);
+        return function (this: Patched, ...args: unknown[]): void {
+            if (Date.now() >= this[timeToNextCall]) {
+                this[timeToNextCall] = Date.now() + convertFrom(time, UnitOfTime.Millisecond, unit);
+                return target.call(this, ...args) as Return;
             }
             return;
         };
