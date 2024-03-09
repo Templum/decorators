db27462aed9a4e93657fed1369e25c2e848d64f4
Templum <templum.dev@gmail.com>
2024-03-09 12:24:17 +0000

    :recycle: Fixed up throttle

--- lib/execution/throttle.ts
+++ lib/execution/throttle.ts
@@ -1,28 +1,42 @@
 import { UnitOfTime } from '../util/types.js';
 import { convertFrom } from '../util/transfomers.js';
 
+type ThrottleDecorator = <TThis, TArgs extends unknown[], Return extends void>(
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    target: Function,
+    ctx: ClassMethodDecoratorContext<TThis, (this: TThis, ...args: TArgs) => Return>,
+) => void;
+
 /**
  * A Method Decorator that throttles calls being made to the decorated target. Ensuring that
  * calls are at most called once per defined time window
  * @param time value
  * @param unit of the provided timing
  */
-export function Throttle(time: number, unit: Exclude<UnitOfTime, 'Nanosecond'>) {
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    return (target: Function, _ctx: ClassMethodDecoratorContext) => {
-        let pendingTimeout: unknown = undefined;
+export function Throttle(time: number, unit: Exclude<UnitOfTime, 'Nanosecond'>): ThrottleDecorator {
+    return <TThis, TArgs extends unknown[], Return extends void>(
+        // eslint-disable-next-line @typescript-eslint/ban-types
+        target: Function,
+        ctx: ClassMethodDecoratorContext<TThis, (this: TThis, ...args: TArgs) => Return>,
+    ) => {
+        const pendingTimeout = Symbol.for('pendingTimeout');
+        type Patched = TThis & { [pendingTimeout]: unknown };
 
-        return <Fn, Arg, Result>(self: Fn, ...args: Arg[]): Result => {
-            if (pendingTimeout === undefined) {
-                pendingTimeout = setTimeout(
-                    () => {
-                        pendingTimeout = undefined;
-                    },
-                    convertFrom(time, UnitOfTime.Millisecond, unit),
-                );
-                return target.call(self, args) as Result;
+        ctx.addInitializer(function () {
+            (this as Patched)[pendingTimeout] = undefined;
+        });
+
+        const timeoutInMs = convertFrom(time, UnitOfTime.Millisecond, unit);
+
+        return function (this: Patched, ...args: unknown[]): void {
+            if (this[pendingTimeout] === undefined) {
+                this[pendingTimeout] = setTimeout(() => {
+                    this[pendingTimeout] = undefined;
+                }, timeoutInMs);
+
+                return target.call(this, ...args) as Return;
             }
-            return undefined as Result;
+            return;
         };
     };
 }
