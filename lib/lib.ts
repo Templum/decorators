5b984ecbfdd568e8090ddd49c80974fc1d3b683d
Templum <templum.dev@gmail.com>
2024-03-15 11:06:55 +0000

    :recycle: Exporting Decorators

--- lib/lib.ts
+++ lib/lib.ts
@@ -1,5 +1,16 @@
-// TODO: Export Decorators
+// Decorator covering Execution
+export { Debounce } from './execution/debounce.js';
+export { Once } from './execution/once.js';
+export { Retry } from './execution/retry.js';
+export { Throttle } from './execution/throttle.js';
 
-// IDEA: Metric Decorators: Counter, Gauge, Histogram based on Prometheus [https://github.com/siimon/prom-client]
+// Decorator covering Metrics
+export { Counter, CallCounter } from './metrics/counter.js';
+export { Gauge } from './metrics/gauge.js';
+export { Measure } from './metrics/measure.js';
+
+export { MetricBroadcaster } from './metrics/broadcaster.js';
+
+// IDEA: Metric Decorators: Histogram based on Prometheus [https://github.com/siimon/prom-client]
 // IDEA: Cache Decorator: Memory + Adapter
 // IDEA: Rate Limit
